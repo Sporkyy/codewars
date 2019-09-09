a979c31bc6c5a1538a1548c4aaccb1e33cb5aa1a
Todd Sayre <captain.friendly@gmail.com>
2019-09-09 12:09:25 -0400

    Even better

--- 5-kyu-calculating-with-functions.js
+++ 5-kyu-calculating-with-functions.js
@@ -21,16 +21,34 @@
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-const zero = fn => (fn ? fn.call(this, 0, fn) : 0);
-const one = fn => (fn ? fn.call(this, 1, fn) : 1);
-const two = fn => (fn ? fn.call(this, 2, fn) : 2);
-const three = fn => (fn ? fn.call(this, 3, fn) : 3);
-const four = fn => (fn ? fn.call(this, 4, fn) : 4);
-const five = fn => (fn ? fn.call(this, 5, fn) : 5);
-const six = fn => (fn ? fn.call(this, 6, fn) : 6);
-const seven = fn => (fn ? fn.call(this, 7, fn) : 7);
-const eight = fn => (fn ? fn.call(this, 8, fn) : 8);
-const nine = fn => (fn ? fn.call(this, 9, fn) : 9);
+// const zero = fn => (fn ? fn.call(this, 0, fn) : 0);
+// const one = fn => (fn ? fn.call(this, 1, fn) : 1);
+// const two = fn => (fn ? fn.call(this, 2, fn) : 2);
+// const three = fn => (fn ? fn.call(this, 3, fn) : 3);
+// const four = fn => (fn ? fn.call(this, 4, fn) : 4);
+// const five = fn => (fn ? fn.call(this, 5, fn) : 5);
+// const six = fn => (fn ? fn.call(this, 6, fn) : 6);
+// const seven = fn => (fn ? fn.call(this, 7, fn) : 7);
+// const eight = fn => (fn ? fn.call(this, 8, fn) : 8);
+// const nine = fn => (fn ? fn.call(this, 9, fn) : 9);
+
+// const plus = y => x => x + y;
+// const minus = y => x => x - y;
+// const times = y => x => x * y;
+// const dividedBy = y => x => Math.trunc(x / y);
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+const zero = fn => (fn ? fn(0) : 0);
+const one = fn => (fn ? fn(1) : 1);
+const two = fn => (fn ? fn(2) : 2);
+const three = fn => (fn ? fn(3) : 3);
+const four = fn => (fn ? fn(4) : 4);
+const five = fn => (fn ? fn(5) : 5);
+const six = fn => (fn ? fn(6) : 6);
+const seven = fn => (fn ? fn(7) : 7);
+const eight = fn => (fn ? fn(8) : 8);
+const nine = fn => (fn ? fn(9) : 9);
 
 const plus = y => x => x + y;
 const minus = y => x => x - y;
