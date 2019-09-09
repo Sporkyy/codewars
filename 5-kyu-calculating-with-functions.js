b8dbded876899bd24345b7ba62066a240848328a
Todd Sayre <captain.friendly@gmail.com>
2019-09-09 10:31:18 -0400

    Better solutions

--- 5-kyu-calculating-with-functions.js
+++ 5-kyu-calculating-with-functions.js
@@ -3,21 +3,39 @@
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-const zero = s => (s ? eval(`0 ${s}`) : 0);
-const one = s => (s ? eval(`1 ${s}`) : 1);
-const two = s => (s ? eval(`2 ${s}`) : 2);
-const three = s => (s ? eval(`3 ${s}`) : 3);
-const four = s => (s ? eval(`4 ${s}`) : 4);
-const five = s => (s ? eval(`5 ${s}`) : 5);
-const six = s => (s ? eval(`6 ${s}`) : 6);
-const seven = s => (s ? eval(`7 ${s}`) : 7);
-const eight = s => (s ? eval(`8 ${s}`) : 8);
-const nine = s => (s ? eval(`9 ${s}`) : 9);
+// const zero = s => (s ? Math.trunc(eval(`0 ${s}`)) : 0);
+// const one = s => (s ? Math.trunc(eval(`1 ${s}`)) : 1);
+// const two = s => (s ? Math.trunc(eval(`2 ${s}`)) : 2);
+// const three = s => (s ? Math.trunc(eval(`3 ${s}`)) : 3);
+// const four = s => (s ? Math.trunc(eval(`4 ${s}`)) : 4);
+// const five = s => (s ? Math.trunc(eval(`5 ${s}`)) : 5);
+// const six = s => (s ? Math.trunc(eval(`6 ${s}`)) : 6);
+// const seven = s => (s ? Math.trunc(eval(`7 ${s}`)) : 7);
+// const eight = s => (s ? Math.trunc(eval(`8 ${s}`)) : 8);
+// const nine = s => (s ? Math.trunc(eval(`9 ${s}`)) : 9);
 
-const plus = n => `+ ${n}`;
-const minus = n => `- ${n}`;
-const times = n => `* ${n}`;
-const dividedBy = n => `/ ${n}`;
+// const plus = n => `+ ${n}`;
+// const minus = n => `- ${n}`;
+// const times = n => `* ${n}`;
+// const dividedBy = n => `/ ${n}`;
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+const zero = fn => (fn ? fn.call(this, 0, fn) : 0);
+const one = fn => (fn ? fn.call(this, 1, fn) : 1);
+const two = fn => (fn ? fn.call(this, 2, fn) : 2);
+const three = fn => (fn ? fn.call(this, 3, fn) : 3);
+const four = fn => (fn ? fn.call(this, 4, fn) : 4);
+const five = fn => (fn ? fn.call(this, 5, fn) : 5);
+const six = fn => (fn ? fn.call(this, 6, fn) : 6);
+const seven = fn => (fn ? fn.call(this, 7, fn) : 7);
+const eight = fn => (fn ? fn.call(this, 8, fn) : 8);
+const nine = fn => (fn ? fn.call(this, 9, fn) : 9);
+
+const plus = y => x => x + y;
+const minus = y => x => x - y;
+const times = y => x => x * y;
+const dividedBy = y => x => Math.trunc(x / y);
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
@@ -31,3 +49,4 @@ assert.strictEqual(seven(times(five())), 35);
 assert.strictEqual(four(plus(nine())), 13);
 assert.strictEqual(eight(minus(three())), 5);
 assert.strictEqual(six(dividedBy(two())), 3);
+assert.strictEqual(six(dividedBy(four())), 1);
