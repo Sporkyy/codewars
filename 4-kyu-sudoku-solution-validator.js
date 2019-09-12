ea20ec8b60b828708a7006df58d93b3cf454ed3f
Todd Sayre <captain.friendly@gmail.com>
2019-09-12 09:27:45 -0400

    Easies solution; best solution

---
+++ 4-kyu-sudoku-solution-validator.js
@@ -0,0 +1,70 @@
+// 4 kyu | Sudoku Solution Validator
+// https://www.codewars.com/kata/sudoku-solution-validator/
+
+/*
+Sudoku Background
+Sudoku is a game played on a 9x9 grid. The goal of the game is to fill
+all cells of the grid with digits from 1 to 9, so that each column, each
+row, and each of the nine 3x3 sub-grids (also known as blocks) contain
+all of the digits from 1 to 9.
+(More info at: http://en.wikipedia.org/wiki/Sudoku)
+
+Sudoku Solution Validator
+Write a function validSolution/ValidateSolution/valid_solution() that
+accepts a 2D array representing a Sudoku board, and returns true if it is
+a valid solution, or false otherwise. The cells of the sudoku board may
+also contain 0's, which will represent empty cells. Boards containing one
+or more zeroes are considered to be invalid solutions.
+
+The board is always 9 cells by 9 cells, and every cell only contains
+integers from 0 to 9.
+*/
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+const validSolution = board => {
+  const cnts = new Array(10).fill(0);
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      cnts[board[i][j]]++;
+      cnts[board[j][i]]++;
+      cnts[board[j % 3][i % 3]]++;
+      if (0 < cnts[0]) return false;
+    }
+  }
+  return cnts.slice(1).every(n => 27 === n);
+};
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+const assert = require('assert');
+
+assert.strictEqual(
+  validSolution([
+    [5, 3, 4, 6, 7, 8, 9, 1, 2],
+    [6, 7, 2, 1, 9, 5, 3, 4, 8],
+    [1, 9, 8, 3, 4, 2, 5, 6, 7],
+    [8, 5, 9, 7, 6, 1, 4, 2, 3],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    [9, 6, 1, 5, 3, 7, 2, 8, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 4, 5, 2, 8, 6, 1, 7, 9],
+  ]),
+  true,
+);
+
+assert.strictEqual(
+  validSolution([
+    [5, 3, 4, 6, 7, 8, 9, 1, 2],
+    [6, 7, 2, 1, 9, 0, 3, 4, 8],
+    [1, 0, 0, 3, 4, 2, 5, 6, 0],
+    [8, 5, 9, 7, 6, 1, 0, 2, 0],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    [9, 0, 1, 5, 3, 7, 2, 1, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 0, 0, 4, 8, 1, 1, 7, 9],
+  ]),
+  false,
+);
