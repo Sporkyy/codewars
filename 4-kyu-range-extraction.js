3ef4c8b12fb30480a5c1edd0a77e12f5d689dcec
Todd Sayre <captain.friendly@gmail.com>
2019-09-19 10:12:06 -0400

    4 kyu | Range Extraction

--- 4-kyu-range-extraction.js
+++ 4-kyu-range-extraction.js
@@ -7,31 +7,69 @@
  * @param {number[]} list
  * @returns {string}
  */
-const solution = list => {
-  if (list.length <= 2) return list.join(',');
-  let result = [];
-  let temp = [];
-  for (let i = 0; i < list.length; i++) {
-    if (list[i] !== temp[temp.length - 1] + 1) {
-      if (temp.length < 3) {
-        result = result.concat(temp);
-        temp = [];
-      } else {
-        result.push(`${temp.shift()}-${temp.pop()}`);
-        temp = [];
-      }
-    }
-    temp.push(list[i]);
-  }
-  if (temp.length < 3) {
-    result = result.concat(temp);
-    temp = [];
-  } else {
-    result.push(`${temp.shift()}-${temp.pop()}`);
-    temp = [];
-  }
-  return result.join(',');
-};
+// const solution = list => {
+//   if (list.length <= 2) return list.join(',');
+//   let result = [];
+//   let temp = [];
+//   for (let i = 0; i < list.length; i++) {
+//     if (list[i] !== temp[temp.length - 1] + 1) {
+//       if (temp.length < 3) {
+//         result = result.concat(temp);
+//         temp = [];
+//       } else {
+//         result.push(`${temp.shift()}-${temp.pop()}`);
+//         temp = [];
+//       }
+//     }
+//     temp.push(list[i]);
+//   }
+//   if (temp.length < 3) {
+//     result = result.concat(temp);
+//     temp = [];
+//   } else {
+//     result.push(`${temp.shift()}-${temp.pop()}`);
+//     temp = [];
+//   }
+//   return result.join(',');
+// };
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+/**
+ * @param {number[]} list
+ * @returns {string}
+ */
+// const solution = list => {
+//   let result = [];
+//   while (0 < list.length) {
+//     const n = list.shift();
+//     const range = result[result.length - 1] || [];
+//     const expected = range[range.length - 1] + 1;
+//     if (n === expected) range.push(n);
+//     else result.push([n]);
+//   }
+//   return result
+//     .map(a => (a.length < 3 ? a.join(',') : `${a[0]}-${a[a.length - 1]}`))
+//     .join(',');
+// };
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+/**
+ * @param {number[]} list
+ * @returns {string}
+ */
+const solution = list =>
+  list
+    .reduce((acc, curr) => {
+      const range = acc[acc.length - 1] || [];
+      const expected = range[range.length - 1] + 1;
+      if (curr === expected) range.push(curr);
+      else acc.push([curr]);
+      return acc;
+    }, [])
+    .map(a => (a.length < 3 ? a.join(',') : `${a[0]}-${a[a.length - 1]}`))
+    .join(',');
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
