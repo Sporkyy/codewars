1c5b32cd8d07d1fa6bc8cccd9f337fb57ac5910d
Todd Sayre <captain.friendly@gmail.com>
2019-12-18 11:06:59 -0500

    A defensible solution

--- 5-kyu-rot13-1.js
+++ 5-kyu-rot13-1.js
@@ -3,26 +3,49 @@
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-const upper = [...new Array(26).keys()].map(n => String.fromCharCode(n + 65));
-const lower = [...new Array(26).keys()].map(n => String.fromCharCode(n + 97));
+// const upper = [...new Array(26).keys()].map(n => String.fromCharCode(n + 65));
+// const lower = [...new Array(26).keys()].map(n => String.fromCharCode(n + 97));
 
-const upperRotated = [...upper];
-const lowerRotated = [...lower];
+// const upperRotated = [...upper];
+// const lowerRotated = [...lower];
 
-const map = new Map();
+// const map = new Map();
+
+// for (let i = 0; i < 13; i++) {
+//   [upper[i], upper[i + 13]] = [upper[i + 13], upper[i]];
+//   [lower[i], lower[i + 13]] = [lower[i + 13], lower[i]];
+// }
+
+// for (let i = 0; i < 26; i++) {
+//   map.set(upper[i], upperRotated[i]);
+//   map.set(lower[i], lowerRotated[i]);
+// }
+
+// const rot13 = message =>
+//   [...message].map(c => (map.has(c) ? map.get(c) : c)).join('');
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+const rotatedCharCodes = [...new Array(128).keys()];
 
 for (let i = 0; i < 13; i++) {
-  [upper[i], upper[i + 13]] = [upper[i + 13], upper[i]];
-  [lower[i], lower[i + 13]] = [lower[i + 13], lower[i]];
-}
-
-for (let i = 0; i < 26; i++) {
-  map.set(upper[i], upperRotated[i]);
-  map.set(lower[i], lowerRotated[i]);
+  [
+    rotatedCharCodes[65 + i],
+    rotatedCharCodes[65 + i + 13],
+    rotatedCharCodes[97 + i],
+    rotatedCharCodes[97 + i + 13],
+  ] = [
+    rotatedCharCodes[65 + i + 13],
+    rotatedCharCodes[65 + i],
+    rotatedCharCodes[97 + i + 13],
+    rotatedCharCodes[97 + i],
+  ];
 }
 
 const rot13 = message =>
-  [...message].map(c => (map.has(c) ? map.get(c) : c)).join('');
+  [...message]
+    .map(c => String.fromCharCode(rotatedCharCodes[c.charCodeAt(0)]))
+    .join('');
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
@@ -31,8 +54,8 @@ console.log(rot13('test'));
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-import { strictEqual } from 'assert';
+// import { strictEqual } from 'assert';
 
-strictEqual(rot13('test'), 'grfg');
+// strictEqual(rot13('test'), 'grfg');
 
-strictEqual(rot13('Test'), 'Grfg');
+// strictEqual(rot13('Test'), 'Grfg');
