e3974787678153a52bd6ed3b692f5f70bc571ddc
Todd Sayre <captain.friendly@gmail.com>
2020-08-05 11:05:46 -0400

    Updates from root config in home directory

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,25 @@
+/* https://michelelarson.com/prettier-config/ */
+
 module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  insertPragma: false,
+  jsxBracketSameLine: true,
   printWidth: 80,
+  proseWrap: 'always',
+  requirePragma: false,
+  semi: true,
   singleQuote: true,
+  tabWidth: 2,
   trailingComma: 'all',
+  useTabs: false,
+
+  overrides: [
+    {
+      files: '*.html',
+      options: {
+        printWidth: 9999,
+      },
+    },
+  ],
 };
