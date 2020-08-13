0cbf7b93ce1bd5ea933987c5df6db36d64b7cf46
Todd Sayre <captain.friendly@gmail.com>
2020-08-13 14:09:48 -0400

    1-line solution

---
+++ 7-kyu-ones-and-zeros.js
@@ -0,0 +1,46 @@
+// 7 kyu | Ones and Zeros
+// https://www.codewars.com/kata/578553c3a1b8d5c40300037c
+
+/*
+
+Given an array of ones and zeroes, convert the equivalent binary value to an
+integer.
+
+Eg: `[0, 0, 0, 1]` is treated as 0001 which is the binary representation of `1`.
+
+## Examples
+```
+Testing: [0, 0, 0, 1] ==> 1
+Testing: [0, 0, 1, 0] ==> 2
+Testing: [0, 1, 0, 1] ==> 5
+Testing: [1, 0, 0, 1] ==> 9
+Testing: [0, 0, 1, 0] ==> 2
+Testing: [0, 1, 1, 0] ==> 6
+Testing: [1, 1, 1, 1] ==> 15
+Testing: [1, 0, 1, 1] ==> 11
+```
+
+However, the arrays can have varying lengths, not just limited to `4`.
+
+*/
+
+// 〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰
+
+// const binaryArrayToNumber = arr =>
+//   arr.reduce(
+//     (dec, binDig, idx) => dec + binDig * 2 ** (arr.length - 1 - idx),
+//     0,
+//   );
+
+// 〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰
+
+const binaryArrayToNumber = arr => Number.parseInt(arr.join(''), 2);
+
+// 〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰
+
+import { strictEqual } from 'assert';
+
+strictEqual(binaryArrayToNumber([0, 0, 0, 1]), 1);
+strictEqual(binaryArrayToNumber([0, 0, 1, 0]), 2);
+strictEqual(binaryArrayToNumber([1, 1, 1, 1]), 15);
+strictEqual(binaryArrayToNumber([0, 1, 1, 0]), 6);
