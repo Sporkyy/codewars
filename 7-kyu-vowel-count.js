11ec0b7709615739c5959721970a879a4c5eafd3
Todd Sayre <captain.friendly@gmail.com>
2020-08-19 16:58:21 -0400

    1-line solution (well, two of them, actually)

---
+++ 7-kyu-vowel-count.js
@@ -0,0 +1,28 @@
+// 7 kyu | Vowel Count
+// https://www.codewars.com/kata/54ff3102c1bad923760001f3/train/javascript
+
+/*
+
+Return the number (count) of vowels in the given string.
+
+We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+The input string will only consist of lower case letters and/or spaces.
+
+*/
+
+// 〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰
+
+// const getCount = str => (m => (m ? m.length : 0))(str.match(/[aeiou]/gi));
+
+// 〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰
+
+const getCount = str => (str.match(/[aeiou]/gi) || []).length;
+
+// 〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰
+
+import { strictEqual } from 'assert';
+
+strictEqual(getCount('abracadabra'), 5);
+
+strictEqual(getCount('bcd'), 0);
