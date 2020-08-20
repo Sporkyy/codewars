9aa03679b847a2be7608e8885a63d8cc12768bb4
Todd Sayre <captain.friendly@gmail.com>
2020-08-20 10:52:47 -0400

    1-line solution

---
+++ 6-kyu-duplicate-encoder.js
@@ -0,0 +1,48 @@
+// 6 kyu | Duplicate Encoder
+// https://www.codewars.com/kata/54b42f9314d9229fd6000d9c
+
+/*
+
+The goal of this exercise is to convert a string to a new string where each
+character in the new string is "(" if that character appears only once in the
+original string, or ")" if that character appears more than once in the original
+string. Ignore capitalization when determining if a character is a duplicate.
+
+## Examples
+
+```
+"din"      =>  "((("
+"recede"   =>  "()()()"
+"Success"  =>  ")())())"
+"(( @"     =>  "))(("
+```
+
+## Notes
+
+Assertion messages may be unclear about what they display in some languages. If
+you read "...It Should encode XXX", the "XXX" is the expected result, not the
+input!
+
+*/
+
+// 〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰
+
+const duplicateEncode = word =>
+  (lcWord =>
+    [...lcWord]
+      .map(letter =>
+        lcWord.indexOf(letter) === lcWord.lastIndexOf(letter) ? '(' : ')',
+      )
+      .join(''))(word.toLowerCase());
+
+// 〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰
+
+import { strictEqual } from 'assert';
+
+strictEqual(duplicateEncode('din'), '(((');
+strictEqual(duplicateEncode('recede'), '()()()');
+strictEqual(duplicateEncode('Success'), ')())())', 'should ignore case');
+strictEqual(duplicateEncode('(( @'), '))((');
+
+strictEqual(duplicateEncode('SJSH(TSwzPSSGSSS'), ')()((()((())()))');
+strictEqual(duplicateEncode('I!Tu@IIeIIIS'), ')(((())()))(');
