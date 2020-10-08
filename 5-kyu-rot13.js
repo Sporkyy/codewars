3102d299790a61ec6ef14864045c5b443b27cd0e
Todd Sayre <captain.friendly@gmail.com>
2020-10-08 15:43:44 -0400

    A cuople of different approaches

--- 5-kyu-rot13.js
+++ 5-kyu-rot13.js
@@ -24,55 +24,47 @@ rot13("This is my first ROT13 excercise!" == "Guvf vf zl svefg EBG13 rkprepvfr!"
 
 // You have passed all of the tests! :)
 
+// /**
+//  * @param {string} str
+//  * @returns {string}
+//  */
+// const rot13 = str => {
+//   console.log(str);
+//   let rotated = '';
+//   for (let i = 0; i < str.length; i++) {
+//     let code = str.charCodeAt(i);
+//     if (64 < code && code < 91) {
+//       code = code - 13;
+//       if (code < 65) code = 91 - (65 - code);
+//     } else if (96 < code && code < 123) {
+//       code = code - 13;
+//       if (code < 97) code = 123 - (97 - code);
+//     }
+//     rotated += String.fromCharCode(code);
+//   }
+//   return rotated;
+// };
+
+// 〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰
+
 /**
  * @param {string} str
  * @returns {string}
  */
 const rot13 = str => {
-  console.log(str);
-  let rotated = '';
-  for (let i = 0; i < str.length; i++) {
-    let code = str.charCodeAt(i);
-    if (64 < code && code < 91) {
-      code = code - 13;
-      if (code < 65) code = 91 - (65 - code);
-    } else if (96 < code && code < 123) {
-      code = code - 13;
-      if (code < 97) code = 123 - (97 - code);
-    }
-    rotated += String.fromCharCode(code);
-  }
-  return rotated;
+  let normal = 'abcdefghijklmnopqrstuvwxyz';
+  let rotated = `${normal.slice(13, 26)}${normal.slice(0, 13)}`;
+  normal = `${normal}${normal.toUpperCase()}`;
+  rotated = `${rotated}${rotated.toUpperCase()}`;
+  const map = new Map([...normal].map((n, i) => [n, rotated[i]]));
+  return [...str].map(c => map.get(c) || c).join('');
 };
 
 // 〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰
 
 import { strictEqual } from 'assert';
 
-// strictEqual(rot13('EBG13 rknzcyr.'), 'ROT13 example.');
-
-/*
-
-E = 69
-  % 13 = 4
-  91 - (13 - 4) = 82 = R
-
-((x - 1) + k) % k
-
-E = 69
-(69 - 1 + 91) % 91 = 68
-(69 - 13 + 91) % 91 = 56
-
-+---+---+---+---+---+
-| 0 | 1 | 2 | 3 | 4 |
-+---+---+---+---+---+
-| a | b | c | d | e |
-+---+---+---+---+---+
-
-((x - 1) + k) % k
-((2 - 4) + 4) % 4 = 2
-
-*/
+strictEqual(rot13('EBG13 rknzcyr.'), 'ROT13 example.');
 
 strictEqual(
   rot13('This is my first ROT13 excercise!'),
