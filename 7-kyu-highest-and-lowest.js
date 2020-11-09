429b43b6cc0646b0dea57db21ee0a2a68c7dec9b
Todd Sayre <captain.friendly@gmail.com>
2020-11-09 16:47:18 -0500

    Comments formatting

--- 7-kyu-highest-and-lowest.js
+++ 7-kyu-highest-and-lowest.js
@@ -3,7 +3,8 @@
 
 /*
 
-In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+In this little assignment you are given a string of space separated numbers, and
+have to return the highest and lowest number.
 
 ## Example
 
@@ -17,7 +18,8 @@ highAndLow("1 9 3 4 -5"); // return "9 -5"
 
 - All numbers are valid `Int32`, no need to validate them.
 - There will always be at least one number in the input string.
-- Output string must be two numbers separated by a single space, and highest number is first.
+- Output string must be two numbers separated by a single space, and highest
+  number is first.
 
 */
 
